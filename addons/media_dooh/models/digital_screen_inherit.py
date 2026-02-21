f08c1deb421890331b61308aed6d081e79de6e55
athmanziri <issaziri@gmail.com>
2026-02-21 15:10:40 +0300

    [media_dooh] Implement automated slot creation and removal logic

--- addons/media_dooh/models/digital_screen_inherit.py
+++ addons/media_dooh/models/digital_screen_inherit.py
@@ -7,11 +7,39 @@ class MediaDigitalScreen(models.Model):
     slot_duration = fields.Integer(string='Slot Duration (sec)', default=15)
     
     slot_ids = fields.One2many('media.dooh.slot', 'digital_screen_id', string='Digital Slots')
+    order_line_ids = fields.One2many('sale.order.line', 'media_digital_screen_id', string='Order History')
     
     @api.depends('slot_duration', 'number_of_slots')
     def _compute_loop_duration(self):
         for screen in self:
             screen.loop_duration = screen.slot_duration * screen.number_of_slots
+
+    def _set_number_of_slots(self):
+        """ Automatically create or remove slots based on number_of_slots field """
+        for screen in self:
+            current_slots = screen.slot_ids
+            target_count = screen.number_of_slots
+            current_count = len(current_slots)
+            
+            if target_count > current_count:
+                # Create more slots
+                slots_to_create = []
+                for i in range(current_count + 1, target_count + 1):
+                    slots_to_create.append({
+                        'digital_screen_id': screen.id,
+                        'name': 'New',
+                    })
+                if slots_to_create:
+                    self.env['media.dooh.slot'].create(slots_to_create)
+            
+            elif target_count < current_count:
+                # Remove extra available slots to match target
+                # Prioritize removing available ones from the end
+                diff = current_count - target_count
+                available_slots = current_slots.filtered(lambda s: s.state == 'available').sorted(key=lambda s: s.id, reverse=True)
+                
+                slots_to_unlink = available_slots[:diff]
+                slots_to_unlink.unlink()
     
     occupied_slots = fields.Integer(string='Occupied Slots', compute='_compute_slot_counts_and_views', store=True)
     available_slots = fields.Integer(string='Available Slots', compute='_compute_slot_counts_and_views', store=True)
