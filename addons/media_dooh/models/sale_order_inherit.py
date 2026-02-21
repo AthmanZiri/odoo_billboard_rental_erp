d13ee6842a49adee9521a6a0ef824fe5957350d5
athmanziri <issaziri@gmail.com>
2026-02-21 15:11:02 +0300

    [media_dooh] Add booking validation to prevent slot double-booking

--- addons/media_dooh/models/sale_order_inherit.py
+++ addons/media_dooh/models/sale_order_inherit.py
@@ -1,9 +1,28 @@
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     media_slot_id = fields.Many2one('media.dooh.slot', string='Digital Slot')
+    partner_id = fields.Many2one('res.partner', related='order_id.partner_id', string='Customer', store=True)
+
+    @api.constrains('media_slot_id', 'start_date', 'end_date', 'state')
+    def _check_digital_slot_availability(self):
+        for line in self:
+            if not line.media_slot_id or not line.start_date or not line.end_date:
+                continue
+            
+            domain = [
+                ('id', '!=', line.id),
+                ('media_slot_id', '=', line.media_slot_id.id),
+                ('state', 'in', ['sale', 'done']),
+                ('start_date', '<=', line.end_date),
+                ('end_date', '>=', line.start_date),
+            ]
+            overlapping = self.search_count(domain)
+            if overlapping:
+                raise ValidationError(_('The digital slot %s is already booked for the selected period.') % line.media_slot_id.name)
 
     def _prepare_invoice_line(self, **optional_values):
         res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
