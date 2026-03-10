c88b065c188d5f9abc3c6471ae1fa671aea08a69
athmanziri <issaziri@gmail.com>
2026-03-10 12:44:43 +0300

    FIX: Add bottom margin increase to paperformat utility script

--- update_paperformat_margins.py
+++ update_paperformat_margins.py
@@ -16,10 +16,18 @@ PaperFormat = env['report.paperformat']
 formats = PaperFormat.search([])
 
 for pf in formats:
+    updated = False
+    
     if pf.margin_top < 65:
         print(f"Updating {pf.name}: margin_top {pf.margin_top} -> 65, header_spacing {pf.header_spacing} -> 60")
         pf.margin_top = 65
         pf.header_spacing = 60
+        updated = True
+        
+    if pf.margin_bottom < 40:
+        print(f"Updating {pf.name}: margin_bottom {pf.margin_bottom} -> 40")
+        pf.margin_bottom = 40
+        updated = True
 
 env.cr.commit()
 print("SUCCESS - Margins Updated!")
