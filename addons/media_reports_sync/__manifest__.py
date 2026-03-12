cbdc7ce6e1af852b1b7fbcc89ff7d5e55103fc95
athmanziri <issaziri@gmail.com>
2026-03-12 10:32:42 +0300

    ADD addons/media_reports_sync for report customizations

---
+++ addons/media_reports_sync/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Media Reports Sync',
+    'version': '1.0',
+    'category': 'Technical',
+    'summary': 'Unified report customizations for Media ERP',
+    'author': 'JengaSol Consulting',
+    'website': 'https://jengasol.co.ke',
+    'depends': ['sale', 'account', 'media_inventory', 'media_finance'],
+    'data': [
+        'data/paperformat_data.xml',
+        'report/report_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
