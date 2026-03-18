26743720aa22ca56bb62c3487425c63f78cbdfaf
athmanziri <issaziri@gmail.com>
2026-03-18 08:14:12 +0300

    ADD partenr history module

---
+++ addons/media_partner_history/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Media Partner History',
+    'version': '1.0',
+    'summary': 'View media rentals and printing history in customer profile',
+    'category': 'Marketing/Billboard',
+    'author': 'Antigravity',
+    'depends': ['media_inventory', 'media_finance', 'media_dooh', 'sale', 'sale_management'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ addons/media_partner_history/models/res_partner.py
@@ -0,0 +1,82 @@
+from odoo import models, fields, api, _
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    media_line_ids = fields.One2many('sale.order.line', 'partner_id', string='All Media Lines', domain=[('state', 'in', ['sale', 'done'])])
+    
+    billboard_line_ids = fields.Many2many('sale.order.line', string='Billboard History', compute='_compute_media_history_lines')
+    digital_line_ids = fields.Many2many('sale.order.line', string='Digital History', compute='_compute_media_history_lines')
+    canopy_line_ids = fields.Many2many('sale.order.line', string='Canopy History', compute='_compute_media_history_lines')
+    printing_line_ids = fields.Many2many('sale.order.line', string='Printing History', compute='_compute_media_history_lines')
+
+    billboard_count = fields.Integer(compute='_compute_media_history_lines', string='Billboards')
+    digital_count = fields.Integer(compute='_compute_media_history_lines', string='Digital Slots')
+    canopy_count = fields.Integer(compute='_compute_media_history_lines', string='Canopies')
+    printing_count = fields.Integer(compute='_compute_media_history_lines', string='Printing')
+
+    def _compute_media_history_lines(self):
+        for partner in self:
+            # We fetch all confirmed sale order lines for this partner
+            # that have any of our media-related fields set.
+            # Using order_id.partner_id for robustness if partner_id is not stored.
+            all_lines = self.env['sale.order.line'].search([
+                ('order_id.partner_id', '=', partner.id),
+                ('state', 'in', ['sale', 'done']),
+                '|', '|', '|',
+                ('media_face_id', '!=', False),
+                ('canopy_id', '!=', False),
+                ('media_digital_screen_id', '!=', False),
+                ('media_slot_id', '!=', False)
+            ])
+
+            billboard_lines = []
+            digital_lines = []
+            canopy_lines = []
+            printing_lines = []
+
+            for line in all_lines:
+                if line.canopy_id:
+                    canopy_lines.append(line.id)
+                elif line.media_slot_id or (line.media_face_id and line.media_face_id.face_type == 'digital'):
+                    digital_lines.append(line.id)
+                elif line.media_face_id:
+                    if 'flexi' in (line.media_face_id.name or '').lower():
+                        printing_lines.append(line.id)
+                    else:
+                        billboard_lines.append(line.id)
+                # Fallback check for printing products if any (future proofing)
+                elif line.product_id and 'printing' in (line.product_id.name or '').lower():
+                    printing_lines.append(line.id)
+
+            partner.billboard_line_ids = [(6, 0, billboard_lines)]
+            partner.digital_line_ids = [(6, 0, digital_lines)]
+            partner.canopy_line_ids = [(6, 0, canopy_lines)]
+            partner.printing_line_ids = [(6, 0, printing_lines)]
+            
+            partner.billboard_count = len(billboard_lines)
+            partner.digital_count = len(digital_lines)
+            partner.canopy_count = len(canopy_lines)
+            partner.printing_count = len(printing_lines)
+
+    def action_view_billboard_history(self):
+        return self._action_view_history(self.billboard_line_ids, _('Billboard History'))
+
+    def action_view_digital_history(self):
+        return self._action_view_history(self.digital_line_ids, _('Digital History'))
+
+    def action_view_canopy_history(self):
+        return self._action_view_history(self.canopy_line_ids, _('Canopy History'))
+
+    def action_view_printing_history(self):
+        return self._action_view_history(self.printing_line_ids, _('Printing History'))
+
+    def _action_view_history(self, lines, name):
+        return {
+            'name': name,
+            'type': 'ir.actions.act_window',
+            'res_model': 'sale.order.line',
+            'view_mode': 'list,form',
+            'domain': [('id', 'in', lines.ids)],
+            'context': {'create': False},
+        }
